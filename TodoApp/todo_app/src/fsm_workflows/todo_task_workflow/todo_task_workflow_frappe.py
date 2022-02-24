9a6a5b8f8ad628f544c1787b0f6e5c0af8896256
Elton Lobo <elton1231@gmail.com>
2022-02-24 15:10:29 +0400

    feat: simple ToDo doctype workflow

---
+++ TodoApp/todo_app/src/fsm_workflows/todo_task_workflow/todo_task_workflow_frappe.py
@@ -0,0 +1,35 @@
+import frappe
+from fsm_workflows.todo_task_workflow.todo_task_workflow_status_base import ToDoTaskWorkflowStatus
+from renovation.utils.fsm import FrappeDocumentFSM
+from transitions import Machine
+
+
+class ToDoTaskWorkflowMachineMixin(FrappeDocumentFSM):
+    """
+    Define all FSM related configurations here
+    """
+    status_class = ToDoTaskWorkflowStatus
+
+    def __init__(self, *args, **kwargs):
+        super(ToDoTaskWorkflowMachineMixin, self).__init__(*args, **kwargs)
+        self.machine = Machine(
+            model=None,
+            initial=self.wf_state or self.status_class.SM_INITIAL_STATE,
+            finalize_event='wf_finalize',
+            auto_transitions=False,
+            **self.status_class.get_kwargs()  # noqa: C815
+        )
+
+    @property
+    def state(self):
+        """Get the items workflow state or the initial state if none is set."""
+        return self.wf_state or self.machine.initial
+
+    @state.setter
+    def state(self, value):
+        """Set the items workflow state."""
+        self.wf_state = value
+
+    def wf_finalize(self, *args, **kwargs):
+        """Run this on all transitions."""
+        self.wf_date = frappe.utils.now()
---
+++ TodoApp/todo_app/src/fsm_workflows/todo_task_workflow/todo_task_workflow_status_base.py
@@ -0,0 +1,68 @@
+from renovation.utils.fsm import StatusBase
+
+
+class ToDoTaskWorkflowStatus(StatusBase):
+    # Define the states as constants
+    DRAFT = 'Draft'
+    ACTIVE = 'Active'
+    COMPLETED = 'Completed'
+    DELETED = 'Deleted'
+
+    # Give the states a human readable label
+    STATE_CHOICES = (
+        (DRAFT, 'Editable ToDo'),
+        (ACTIVE, 'Active ToDo'),
+        (COMPLETED, 'Completed ToDo'),
+        (DELETED, 'Deleted ToDo'),
+    )
+
+    # Define the transitions as constants
+    ACTIVATE = 'activate_todo'
+    MARK_COMPLETED = 'make_completed'
+    MARK_DELETED = 'mark_deleted'
+    REVERT_DELETED = 'revert_delete'
+
+    # Give the transitions a human readable label
+    TRANSITION_LABELS = {
+        ACTIVATE: {'label': 'Make ToDo active'},
+        MARK_COMPLETED: {'label': 'Mark ToDo as completed'},
+        MARK_DELETED: {'label': 'Mark ToDo as deleted'},
+        REVERT_DELETED: {'label': 'Revert Deleted ToDo'},
+    }
+
+    # Construct the values to pass to the state machine constructor
+
+    # The states of the machine
+    SM_STATES = [
+        DRAFT, ACTIVE, COMPLETED, DELETED,
+    ]
+
+    # The machines initial state
+    SM_INITIAL_STATE = DRAFT
+
+    # The transitions as a list of dictionaries
+    SM_TRANSITIONS = [
+        # trigger, source, destination
+        {
+            'trigger': ACTIVATE,
+            'source': DRAFT,
+            'dest': ACTIVE,
+        },
+        {
+            'trigger': MARK_COMPLETED,
+            'source': DRAFT,
+            'dest': COMPLETED,
+        },
+        {
+            'trigger': MARK_DELETED,
+            'source': [
+                ACTIVE, COMPLETED, DRAFT
+            ],
+            'dest': DELETED
+        },
+        {
+            'trigger': REVERT_DELETED,
+            'source': DELETED,
+            'dest': DRAFT,
+        },
+    ]
