c0894bc810398955acca8fcfdcb0c58b00257d0a
Elton Lobo <elton1231@gmail.com>
2022-02-27 17:04:03 +0400

    fix: edit todo

--- TodoApp/todo_app/src/todo_app/api/todo_task_workflow/edit_todo.py
+++ TodoApp/todo_app/src/todo_app/api/todo_task_workflow/edit_todo.py
@@ -2,6 +2,7 @@ from todo_app.api import router
 import todo_app.controllers as todo_controllers
 
 
-@router.put("/edit-todo/{todo_name}")
-async def edit_todo(todo_name: str, task_description=None, task_name=None):
-    return await todo_controllers.edit_todo(todo_name, task_name, task_description)
+@router.put("/edit-todo/{todo_name}/")
+async def edit_todo(todo_name: str, update: dict):
+    return await todo_controllers.edit_todo(todo_name, update.get('task_name'),
+                                            update.get('task_description'))
