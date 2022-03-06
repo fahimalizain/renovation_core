f8e54256746c0fcefc689a7ab159915f8669db87
Fahim Ali Zain <fahimalizain@gmail.com>
2022-03-06 11:11:18 +0530

    fix: wrap renovation.set_user

--- renovation_core/renovation/__init__.py
+++ renovation_core/renovation/__init__.py
@@ -7,13 +7,15 @@ from .orm import Field, Column  # noqa
 from .model import FrappeModel as RenovationModel  # noqa
 
 # Useful utilities
-from frappe import (local, _, parse_json, _dict, get_module, get_hooks, get_traceback, scrub)  # noqa
+from frappe import (local, _, parse_json, _dict, get_module,  # noqa
+     get_hooks, get_traceback, scrub, set_user)  # noqa
 
 
 def get_attr(method_string):
     """Get python method object from its name."""
     # app_name = method_string.split(".")[0]
-    # if not local.flags.in_uninstall and not local.flags.in_install and app_name not in get_installed_apps():
+    # if not local.flags.in_uninstall and not local.flags.in_install and \
+    #       app_name not in get_installed_apps():
     # 	throw(_("App {0} is not installed").format(app_name), AppNotInstalledError)
 
     modulename = '.'.join(method_string.split('.')[:-1])
