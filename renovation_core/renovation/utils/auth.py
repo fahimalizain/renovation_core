60001a51bba9a4e67c29f6c340fc60e390902ede
Fahim Ali Zain <fahimalizain@gmail.com>
2022-03-30 14:17:03 +0530

    feat: get_bearer_token

---
+++ renovation_core/renovation/utils/auth.py
@@ -0,0 +1,82 @@
+import frappe
+from frappe.utils import get_url
+import asyncer
+
+
+def get_oath_client():
+    client = frappe.db.get_value("OAuth Client", {})
+    if not client:
+        # Make one auto
+        client = frappe.get_doc(frappe._dict(
+            doctype="OAuth Client",
+            app_name="default",
+            scopes="all openid",
+            redirect_urls=get_url(),
+            default_redirect_uri=get_url(),
+            grant_type="Implicit",
+            response_type="Token"
+        ))
+        client.insert(ignore_permissions=True)
+    else:
+        client = frappe.get_doc("OAuth Client", client)
+
+    return client
+
+
+async def get_bearer_token(user, expires_in=3600):
+    # TODO: Make this more async
+    import hashlib
+    import jwt
+    import frappe.oauth
+    from oauthlib.oauth2.rfc6749.tokens import random_token_generator, OAuth2Token  # noqa
+
+    client = await asyncer.asyncify(get_oath_client)()
+    token = frappe._dict({
+        'access_token': random_token_generator(None),
+        'expires_in': expires_in,
+        'token_type': 'Bearer',
+        'scopes': client.scopes,
+        'refresh_token': random_token_generator(None)
+    })
+    bearer_token = frappe.new_doc("OAuth Bearer Token")
+    bearer_token.client = client.name
+    bearer_token.scopes = token['scopes']
+    bearer_token.access_token = token['access_token']
+    bearer_token.refresh_token = token.get('refresh_token')
+    bearer_token.expires_in = token['expires_in'] or 3600
+    bearer_token.user = user
+    bearer_token.save(ignore_permissions=True)
+    frappe.db.commit()
+
+    # ID Token
+    id_token_header = {
+        "typ": "jwt",
+        "alg": "HS256"
+    }
+    id_token = {
+        "aud": "token_client",
+        "exp": int(
+            (frappe.db.get_value(
+                "OAuth Bearer Token",
+                token.access_token,
+                "expiration_time") -
+                frappe.utils.datetime.datetime(
+                1970,
+                1,
+                1)).total_seconds()),
+        "sub": frappe.db.get_value(
+            "User Social Login",
+            {
+                "parent": bearer_token.user,
+                "provider": "frappe"},
+            "userid"),
+        "iss": "frappe_server_url",
+        "at_hash": frappe.oauth.calculate_at_hash(
+            token.access_token,
+            hashlib.sha256)}
+    id_token_encoded = jwt.encode(
+        id_token, "client_secret", algorithm='HS256', headers=id_token_header)
+    id_token_encoded = frappe.safe_decode(id_token_encoded)
+    token.id_token = id_token_encoded
+    frappe.flags.jwt = id_token_encoded
+    return token
